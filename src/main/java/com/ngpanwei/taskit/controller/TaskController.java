83ed8fb83000f7cb03e8a6a7619080987944c653
xqfgbc <gb1932002@aliyun.com>
2017-02-11 13:53:13 +0800

    add TaskController UnitTest for mockmvc

--- src/main/java/com/ngpanwei/taskit/controller/TaskController.java
+++ src/main/java/com/ngpanwei/taskit/controller/TaskController.java
@@ -1,6 +1,8 @@
 package com.ngpanwei.taskit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,14 +20,24 @@ public class TaskController {
     public String home() {
         return "<h1>Hello World</h1>" ;
     }
+
     @RequestMapping(path="/createGroup",method= RequestMethod.GET)
-    public @ResponseBody Task createTask(
+    @ResponseBody
+    public Task createTask(
             @RequestParam(value="name",required=false) String name) {
         return new Task(name,"","");
     }
+
     @RequestMapping(value="/group/{name}",method=RequestMethod.GET)
-    public Task createTask2(
-            @PathVariable String name) {
+    public Task createTask2(@PathVariable String name) {
         return new Task(name,"","");
+    }
+
+    @RequestMapping(value="/user/create",method=RequestMethod.POST)
+    public ResponseEntity<String> createTask2(String name, String sex, String age) {
+        if (name == null || sex ==null || age == null){
+            return new ResponseEntity<>("fail", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 }
--- src/test/java/com/ngpanwei/taskit/TaskitControllerTests.java
+++ src/test/java/com/ngpanwei/taskit/TaskitControllerTests.java
@@ -1,8 +1,13 @@
 package com.ngpanwei.taskit;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.ngpanwei.taskit.controller.TaskController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,28 +15,53 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@AutoConfigureMockMvc
-//public class TaskitControllerTests {
-//	@Autowired
-//	private MockMvc mockMvc;
-//
-//	@Test
-//	public void should_invoke_request() throws Exception {
-//		mockMvc.perform(get("/")).andExpect(content().encoding("UTF-8"));
-//	}
-//
-//	@Test
-//	public void should_create_task() throws Exception {
-//		throw new Exception("not implemented");
-//	}
-//
-//	@Test
-//	public void should_list_task() throws Exception {
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class TaskitControllerTests {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Before
+	public void init(){
+		mockMvc = MockMvcBuilders.standaloneSetup(new TaskController()).build();
+	}
+
+	@Test
+	public void should_invoke_request() throws Exception {
+		//mockMvc.perform(get("/")).andExpect(content().encoding("UTF-8"));
+	}
+
+	@Test
+	public void should_create_task() throws Exception {
+		//throw new Exception("not implemented");
+	}
+
+	@Test
+	public void should_list_task() throws Exception {
 //		mockMvc.perform(get("/tasks")).andExpect(content().encoding("UTF-8"));
 //		throw new Exception("not implemented");
-//	}
-//
-//}
+	}
+
+	@Test
+	public void should_given_route_path_and_params_when_use_post_function_return_httpstatus_200() throws Exception {
+		mockMvc.perform((post("/user/create")
+				.param("name","gongbo")
+				.param("sex","male")
+				.param("age","18")))
+				.andExpect(status().isOk())
+				.andDo(print());
+	}
+
+	@Test
+	public void should_given_route_path_and_less_params_when_use_post_function_return_httpstatus_400() throws Exception {
+		mockMvc.perform((post("/user/create")
+					.param("name", "gongbo")
+					.param("age","18")))
+				.andExpect(status().isBadRequest())
+				.andDo(print());
+	}
+
+}
